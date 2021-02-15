1963befe1b7781c813b841342be34c0d5492e4c8
Donfreddy <freddytamwo@gmail.com>
2021-02-15 17:31:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Token 
+ * eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDY0Mjg0MzczNDI5MjU2MTkzIiwibGltaXQiOjEwMCwia2V5IjoiZFdqU3I5YnF4c0swQmk3ZnEzQnpPWjRPT1Z0WTZsTXZ0bTVDNW1CcWZsRHZCeGdtWkgiLCJjcmVhdGVkX2F0IjoiMjAyMS0wMi0xNVQxNjowMTowMiswMDowMCIsImlhdCI6MTYxMzQwNDg2Mn0.bl--hUzt446V2UQkQfAtyZ_VNTh034HNjrZZJ0RDnGE
+ */
+
+console.log("hello fred !");
+
+export async function cli(args) {}
